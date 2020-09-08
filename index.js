f4e8200e58146fa69abb36da98cc74c2adfad4d5
vincentservio <vincentservio@gmail.com>
2020-09-08 12:44:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr,func){
+    const newArr = []
+    for (let i=0; i < arr.length; i++) {
+        newArr.push(func(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(arr, func, start=0) {
+    let rt = !!start ? start : arr[0]
+    let i = !!start ? 0 : 1
+    for ( ; i < arr.length; i++) {
+        rt = func(arr[i], rt)
+    }
+    return rt
+}
